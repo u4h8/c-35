c8f936b83fd7271099ef07260dde882a75e88017
u4h8 <diso1323@gmail.com>
2021-02-15 18:03:39 -0800

    msg

--- sketch.js
+++ sketch.js
@@ -1,29 +1,47 @@
 var ball;
+var database;
+var position;
 
 function setup(){
     createCanvas(500,500);
+    database = firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballposition = database.ref("ball/position");
+    ballposition.on("value",readposition, showerror)
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+  //  ball.x = ball.x + x;
+  //  ball.y = ball.y + y;
+  database.ref("ball/position").set({
+      x: position.x +x,
+      y: position.y +y
+  })
+}
+function showerror(){
+console.log("error")
+}
+function readposition(data){
+position = data.val();
+console.log(position)
+ball.x = position.x;
+ball.y = position.y;
 }
